1d3983ab7e20e1c23e0b02ed6dc0287d1c5434c5
LEEJ0NGWAN <lastffx@gmail.com>
2021-05-29 13:52:58 +0900

    feat: first init

---
+++ src/main/java/com/kimoota/kimootaapiservice/KimootaApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kimoota.kimootaapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimootaApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimootaApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimoota/kimootaapiservice/KimootaApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimoota.kimootaapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimootaApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
