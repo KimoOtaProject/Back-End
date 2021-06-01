b19d84861785e2374d8c614a5afef3676a4cf167
LEEJ0NGWAN <lastffx@gmail.com>
2021-06-01 17:31:00 +0900

    feat: user repository

---
+++ src/main/java/com/kimoota/kimootaapiservice/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.kimoota.kimootaapiservice.repository;
+
+import com.kimoota.kimootaapiservice.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    User save(User user);
+
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+
+    List<User> findByName(String name);
+    List<User> findAll();
+
+    void deleteById(Long id);
+
+}
