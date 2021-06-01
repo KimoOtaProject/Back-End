d5272ce08722cc64972b42c89f0354c80b126384
LEEJ0NGWAN <lastffx@gmail.com>
2021-06-01 15:45:20 +0900

    feat: base time entity

---
+++ src/main/java/com/kimoota/kimootaapiservice/domain/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.kimoota.kimootaapiservice.domain;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate; // 생성 시 자동 저장
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate; // 수정 시 자동 저장
+}
