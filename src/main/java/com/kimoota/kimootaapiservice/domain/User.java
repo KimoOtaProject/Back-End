3d9f806ebfab92b24dc9e00a527234b451eb3df3
LEEJ0NGWAN <lastffx@gmail.com>
2021-06-01 18:26:59 +0900

    refactor: username -> name 필드명 변경

--- src/main/java/com/kimoota/kimootaapiservice/domain/User.java
+++ src/main/java/com/kimoota/kimootaapiservice/domain/User.java
@@ -5,13 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import java.util.Collection;
 
 @Entity
 @Getter
@@ -27,7 +24,7 @@ public class User extends BaseTimeEntity {
     private String email;
 
     @Column(nullable = false)
-    private String username;
+    private String name;
 
     @JsonIgnore
     @Column(nullable = false)
@@ -44,20 +41,20 @@ public class User extends BaseTimeEntity {
     private String providerId;
 
     @Builder
-    public User(String email, String username, String password) {
+    public User(String email, String name, String password) {
         this.email = email;
-        this.username = username;
+        this.name = name;
         this.password = password;
     }
 
-    public void update(String username) {
-        this.username = username;
+    public void update(String name) {
+        this.name = name;
     }
 
     public User toEntity() {
         return User.builder()
                 .email(email)
-                .username(username)
+                .name(name)
                 .password(password)
                 .build();
     }
