c2bfb0cd233b338b229003bd93ecb9ba7ea01311
LEEJ0NGWAN <lastffx@gmail.com>
2021-06-07 18:24:04 +0900

    feat: auth2 param

---
+++ src/main/java/com/kimoota/kimootaapiservice/param/oauth2/OAuth2UserParam.java
@@ -0,0 +1,28 @@
+package com.kimoota.kimootaapiservice.param.oauth2;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+// 사용자 데이터 파라매터
+@Getter
+public abstract class OAuth2UserParam {
+
+    protected Map<String, Object> attributes;
+
+    @Builder
+    public OAuth2UserParam(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getName();
+
+    public abstract String getEmail();
+
+    public abstract String getImageUrl();
+
+
+}
