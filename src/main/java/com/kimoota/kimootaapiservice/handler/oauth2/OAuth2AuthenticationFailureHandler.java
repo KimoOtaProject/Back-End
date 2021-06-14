ee81e17637ed28c39508116858f5a68bf9fa5d04
LEEJ0NGWAN <lastffx@gmail.com>
2021-06-14 23:24:36 +0900

    style: oauth2 auth failure handler 주석 추가

--- src/main/java/com/kimoota/kimootaapiservice/handler/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/kimoota/kimootaapiservice/handler/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -8,7 +8,6 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationFa
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,12 +20,14 @@ import static com.kimoota.kimootaapiservice
         .HttpCookieOAuth2AuthorizationRequestRepository
         .REDIRECT_URI_PARAM_COOKIE_NAME;
 
+// OAuth2 인증 실패 시 호출되는 핸들러 -> 실패 시 onAuthenticationFailure 메소드 호출
 @Component
 @RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
+    // OAuth2 인증 실패 시 호출
     @Override
     public void onAuthenticationFailure(
             HttpServletRequest request,
